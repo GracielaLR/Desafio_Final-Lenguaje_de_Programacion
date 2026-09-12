a33f2d09ea818d42aeea66a96ce1bf61170f96c3
alejandrohdominguez <N00572159@upn.pe>
2026-09-12 01:20:02 -0500

    Se agrega Atributos de clase AtencionMedica, lista detallesReceta y métodos agregarDetalleReceta y generarInforme

--- SistemaRural_PE/src/modelo/AtencionMedica.java
+++ SistemaRural_PE/src/modelo/AtencionMedica.java
@@ -1,5 +1,38 @@
 package modelo;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AtencionMedica {
+	
+ // Atributos de clase AtencionMedica
+ private String idAtencion;
+ private LocalDateTime fechaAtencion;
+ private String diagnostico;
+ private String tratamiento;
 
+ private List<DetalleReceta> detallesReceta = new ArrayList<>();
+
+ public AtencionMedica(String id, LocalDateTime fecha, String diag, String trat) {
+     this.idAtencion = id;
+     this.fechaAtencion = fecha;
+     this.diagnostico = diag;
+     this.tratamiento = trat;
+ }
+
+ public void agregarDetalleReceta(DetalleReceta detalle) {
+     if(detalle.validarStock()) {
+         detallesReceta.add(detalle);
+         detalle.getMedicamento().actualizarStock(-detalle.getCantidad());
+     }
+ }
+
+ public String generarInforme() {
+     
+     String medStr = detallesReceta.stream()
+         .map(d -> d.getMedicamento().getNombre() + " (Cant: " + d.getCantidad() + ")")
+         .collect(Collectors.joining(", "));
+     return "Fecha: " + fechaAtencion + " | Diagnóstico Oculto (Privacidad) | Medicación: " + medStr;
+ }
 }
