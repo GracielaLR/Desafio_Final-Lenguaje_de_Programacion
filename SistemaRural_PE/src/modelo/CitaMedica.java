d793e39c39b9fab66787b14abb1c62923756ae09
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-12 01:04:18 -0500

    feat: implementar estados y flujo de atención en clase CitaMedica

--- SistemaRural_PE/src/modelo/CitaMedica.java
+++ SistemaRural_PE/src/modelo/CitaMedica.java
@@ -1,5 +1,34 @@
 package modelo;
 
+import java.time.LocalDateTime;
+
 public class CitaMedica {
+	
+	public enum EstadoCita { PENDIENTE, ATENDIDA, CANCELADA } // Integrado para no crear clases extra
+
+    private String idCita;
+    private LocalDateTime fechaHora;
+    private EstadoCita estado;
+    private String motivoConsulta;
+
+    private AtencionMedica atencionMedica; // 0..1 AtencionMedica
+    
+    public void programarCita() {
+    	this.estado = EstadoCita.PENDIENTE;
+    	
+    }
+    
+    public void cancelarCita() {
+    	this.estado = EstadoCita.CANCELADA;
+    	
+    }
+    
+    public AtencionMedica registrarAtencion() {
+        this.estado = EstadoCita.ATENDIDA;
+        this.atencionMedica = new AtencionMedica(this.idCita + "-A", LocalDateTime.now(), "Pendiente de evaluación", "Sin tratamiento inicial");
+        return this.atencionMedica;
+    }
+    
+    public EstadoCita getEstado() {return estado; }
 
 }
