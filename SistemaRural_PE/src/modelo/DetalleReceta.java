a08de4438cfcfaaff86984bc9dc8c8ada9052911
GracielaLR <N00560399@upn.pe>
2026-09-12 01:14:17 -0500

    feat: implementar la vinculacion de medicamento en DetalleReceta

--- SistemaRural_PE/src/modelo/DetalleReceta.java
+++ SistemaRural_PE/src/modelo/DetalleReceta.java
@@ -1,5 +1,20 @@
 package modelo;
 
+//Clase que representa el detalle individual de un medicamento recetado.
+//Actúa como parte de la receta vinculando una cantidad y dosis con un producto.
 public class DetalleReceta {
 
+	private int cantidad;
+    private String indicaciones;
+    private Medicamento medicamento; // Relación 1 Medicamento
+
+    //Verifica si el medicamento recetado cumple con las condiciones para ser despachado.
+    //Evalúa tres reglas de negocio esenciales del sistema de salud.
+    public boolean validarStock() {
+        return medicamento != null && medicamento.getStockDisponible() >= cantidad && !medicamento.estaVencido();
+    }
+
+    //La cantidad recetada del medicamento.
+    public Medicamento getMedicamento() { return medicamento; }
+    public int getCantidad() { return cantidad; }
 }
