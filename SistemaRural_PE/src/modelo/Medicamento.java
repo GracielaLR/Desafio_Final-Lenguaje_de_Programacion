2e10bb44e145c73727eaf206d2ff4daae099598f
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-12 01:10:37 -0500

    feat: implementar control de stock y vencimiento en clase Medicamento

--- SistemaRural_PE/src/modelo/Medicamento.java
+++ SistemaRural_PE/src/modelo/Medicamento.java
@@ -1,5 +1,26 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Medicamento {
 
+	private String idMedicamento;
+    private String nombre;
+    private int stockDisponible;
+    private LocalDate fechaVencimiento;
+	
+    public void actualizarStock(int cant) {
+    	this.stockDisponible += cant;
+    }
+    
+    public boolean estaVencido() {
+    	return LocalDate.now().isAfter(fechaVencimiento);
+    	
+    }
+    
+    public String getNombre() {return nombre;}
+    
+    public int getStockDisponible() {return stockDisponible;}
+    
+    
 }
