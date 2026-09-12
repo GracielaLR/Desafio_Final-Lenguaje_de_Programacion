ace5f8fab039e9f0fe5613eab996aebf8986365e
alejandrohdominguez <N00572159@upn.pe>
2026-09-12 00:56:13 -0500

    Agregar los métodos consultarHistorial y solicitarCita

--- SistemaRural_PE/src/modelo/Paciente.java
+++ SistemaRural_PE/src/modelo/Paciente.java
@@ -8,9 +8,27 @@ import java.util.stream.Collectors;
 public class Paciente extends Persona {
  private String numeroHistoriaClinica;
  private String tipoSeguro;
- private List<CitaMedica> citasMedicas = new ArrayList<>(); // 1..* CitaMedica
+ private List<CitaMedica> citasMedicas = new ArrayList<>(); 
 
+ 
+ //Clase solicitarCita con parámetro objeto de CitaMedica
+ //Agregar objeto de clase a la Lista citasMedicas
+ public void solicitarCita(CitaMedica cita) {
+     citasMedicas.add(cita);
+ }
 
+ 
+ //Agregar un método público consultarHistorial
+ public List<CitaMedica> consultarHistorial() {
+     // tomar la lista de citasMedicas y evaluarla con Stream
+     return citasMedicas.stream()
+    		 
+    	// Considera solo las listas con estado ATENDIDA
+         .filter(cita -> cita.getEstado() == CitaMedica.EstadoCita.ATENDIDA)
+         
+        // Colecciona los elemntos en la lista creada
+         .collect(Collectors.toList());
+ }
 }
 
 
