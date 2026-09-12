6f89642019d52366c53a1d971e1a47446fc84e0e
GracielaLR <N00560399@upn.pe>
2026-09-12 01:06:59 -0500

    feat: Implementar el proceso de citamedica y emitirReceta

--- SistemaRural_PE/src/modelo/Medico.java
+++ SistemaRural_PE/src/modelo/Medico.java
@@ -1,5 +1,24 @@
 package modelo;
 
-public class Medico {
+import java.util.ArrayList;
+import java.util.List;
 
+// Clase que representa a un profesional de la salud en el sistema rural.
+// Aplica el principio de HERENCIA al extender de la clase abstracta Persona.
+
+public class Medico extends Persona {
+	
+	private String cmp;// Registro del Colegio Médico del Perú
+	private String especialidad;
+	private List<CitaMedica> citasAsignadas = new ArrayList<>(); // 1..* CitaMedica
+
+	//Procesa una cita médica pendiente y genera su respectiva atención.
+	public void atenderCita(CitaMedica cita) {
+     cita.registrarAtencion();
+ }
+
+	//Vincula un medicamento recetado a una atención médica específica.
+	public void emitirReceta(AtencionMedica atencion, DetalleReceta detalle) {
+     atencion.agregarDetalleReceta(detalle);
+ }
 }
