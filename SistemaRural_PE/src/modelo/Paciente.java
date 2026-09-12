707f70b605fa866e17ad81efbb134a3a5d2cae98
alejandrohdominguez <N00572159@upn.pe>
2026-09-12 00:45:54 -0500

    Agregar atributos de clase Paciente que hereda de Persona

--- SistemaRural_PE/src/modelo/Paciente.java
+++ SistemaRural_PE/src/modelo/Paciente.java
@@ -1,10 +1,16 @@
 package modelo;
 
-public class Paciente {
-	
-	
-	
-	
-	
-	
+//2. Paciente.java
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Paciente extends Persona {
+ private String numeroHistoriaClinica;
+ private String tipoSeguro;
+ private List<CitaMedica> citasMedicas = new ArrayList<>(); // 1..* CitaMedica
+
+
 }
+
+
